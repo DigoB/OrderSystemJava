6261be6a22e11e4ee58b5b530c0d01d2d4b56853
Rodrigo Braz <braz.rodrigo94@gmail.com>
2023-12-20 13:20:55 -0300

    feat: City and State Entities and Repositories

--- src/main/java/br/com/rodrigobraz/OrderSystemJava/OrderSystemJavaApplication.java
+++ src/main/java/br/com/rodrigobraz/OrderSystemJava/OrderSystemJavaApplication.java
@@ -1,9 +1,13 @@
 package br.com.rodrigobraz.OrderSystemJava;
 
+import br.com.rodrigobraz.OrderSystemJava.controllers.City;
+import br.com.rodrigobraz.OrderSystemJava.controllers.State;
 import br.com.rodrigobraz.OrderSystemJava.entities.Category;
 import br.com.rodrigobraz.OrderSystemJava.entities.Product;
 import br.com.rodrigobraz.OrderSystemJava.repositories.CategoryRepository;
+import br.com.rodrigobraz.OrderSystemJava.repositories.CityRepository;
 import br.com.rodrigobraz.OrderSystemJava.repositories.ProductRepository;
+import br.com.rodrigobraz.OrderSystemJava.repositories.StateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -21,6 +25,12 @@ public class OrderSystemJavaApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
+
+	@Autowired
+	private StateRepository stateRepository;
+
+	@Autowired
+	private CityRepository cityRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderSystemJavaApplication.class, args);
@@ -45,5 +55,19 @@ public class OrderSystemJavaApplication implements CommandLineRunner {
 
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		State state1 = new State(null, "Minas Gerais");
+		State state2 = new State(null,"São Paulo");
+
+		City city1 = new City(null, "Uberlandia", state1);
+		City city2 = new City(null, "São Paulo", state2);
+		City city3 = new City(null, "Campinas", state2);
+
+		state1.getCities().addAll(Arrays.asList(city1));
+		state1.getCities().addAll(Arrays.asList(city2, city3));
+
+		stateRepository.saveAll(Arrays.asList(state1, state2));
+		cityRepository.saveAll(Arrays.asList(city1, city2, city3));
+
 	}
 }
---
+++ src/main/java/br/com/rodrigobraz/OrderSystemJava/repositories/StateRepository.java
@@ -0,0 +1,9 @@
+package br.com.rodrigobraz.OrderSystemJava.repositories;
+
+import br.com.rodrigobraz.OrderSystemJava.controllers.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StateRepository extends JpaRepository<State, Integer> {
+}
