eb38f55abfabf95ddb87fa15c62b8422b0b3248f
Rodrigo Braz <braz.rodrigo94@gmail.com>
2024-02-19 18:57:03 -0300

    feat: Customized validation error

--- src/main/java/br/com/rodrigobraz/OrderSystemJava/exceptions/ControlerExceptionHandler.java
+++ src/main/java/br/com/rodrigobraz/OrderSystemJava/exceptions/ControlerExceptionHandler.java
@@ -3,6 +3,8 @@ package br.com.rodrigobraz.OrderSystemJava.exceptions;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,5 +22,17 @@ public class ControlerExceptionHandler {
         StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), exception.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException exception, HttpServletRequest request) {
+        ValidationError error = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Validation Error", System.currentTimeMillis());
+
+        for (FieldError x : exception.getBindingResult().getFieldErrors()) {
+            error.addError(x.getField(), x.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
 
 }
---
+++ src/main/java/br/com/rodrigobraz/OrderSystemJava/exceptions/ValidationError.java
@@ -0,0 +1,21 @@
+package br.com.rodrigobraz.OrderSystemJava.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+    private static final long serialVersionUID = 1L;
+
+    private List<FieldMessage> errors = new ArrayList<>();
+    public ValidationError(Integer status, String errorMessage, Long timestamp) {
+        super(status, errorMessage, timestamp);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String message) {
+        errors.add(new FieldMessage(fieldName, message));
+    }
+}
