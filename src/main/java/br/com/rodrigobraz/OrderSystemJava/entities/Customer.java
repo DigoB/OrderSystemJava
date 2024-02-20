63d9ccae5828c0fa50c8e401aa3d075a92edde72
Rodrigo Braz <braz.rodrigo94@gmail.com>
2024-02-20 16:08:43 -0300

    feat: DELETE endpoint for Customer using cascade to delete addresses

--- src/main/java/br/com/rodrigobraz/OrderSystemJava/entities/Customer.java
+++ src/main/java/br/com/rodrigobraz/OrderSystemJava/entities/Customer.java
@@ -19,7 +19,7 @@ public class Customer implements Serializable {
     private String document;
     private Integer type;
 
-    @OneToMany(mappedBy="customer")
+    @OneToMany(mappedBy="customer", cascade = CascadeType.ALL)
     private List<Address> addresses = new ArrayList<>();
 
     @ElementCollection
